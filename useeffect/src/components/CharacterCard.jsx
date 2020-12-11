988f8e9e1d002babb8ee8c1081b995be9b9becfc
Tommy <tommykindle83@gmail.com>
2020-12-10 20:22:51 -0800

    pushed changes thursday

--- useeffect/src/components/CharacterCard.jsx
+++ useeffect/src/components/CharacterCard.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
 export default function CharacterCard(props) {
-
-  const {character, name, image, origin, gender, species} = props;
+  const { character } = props;
+  console.log(props);
   return (
     <div>
-    <img src={character.image} alt={character.name}/>
-    <h2>{character.name}</h2>
-
+      <img src={character.image} alt={character.name} />
+      <h2>{character.name}</h2>
+      <p>{character.gender}</p>
+      <p>{character.origin.name}</p>
+      <p>{character.species}</p>
+      <p>{character.status}</p>
     </div>
-  )
+  );
 }
---
+++ useeffect/src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function SearchBar(props) {
+
+
+
+  return (
+    <div>
+    <input type="text"/>
+    <button>Search</button>
+
+    </div>
+  )
+}
